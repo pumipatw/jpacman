b4a26efac069f23d6737533020ed5b742d99a07b
Pumipat Watanakulcharus <pooh99191@gmail.com>
2021-11-09 15:36:42 +0700

    Mockito testparsewrong1

--- src/test/java/nl/tudelft/jpacman/level/MapParserTest.java
+++ src/test/java/nl/tudelft/jpacman/level/MapParserTest.java
@@ -1,7 +1,9 @@
 package nl.tudelft.jpacman.level;
 
+import nl.tudelft.jpacman.PacmanConfigurationException;
 import nl.tudelft.jpacman.board.BoardFactory;
 import nl.tudelft.jpacman.npc.ghost.Blinky;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
@@ -37,6 +39,23 @@ public class MapParserTest {
         map.add("############");
         mapParser.parseMap(map);
         Mockito.verify(levelFactory, Mockito.times(1)).createGhost();
-
+    }
+    /**
+     * Test for the parseMap method (bad map)
+     */
+    @Test
+    public void testParseMapWrong1() {
+        PacmanConfigurationException thrown = Assertions.assertThrows(PacmanConfigurationException.class, () -> {
+           MockitoAnnotations.initMocks(this);
+           assertNotNull(boardFactory);
+           assertNotNull(levelFactory);
+           MapParser mapParser = new MapParser(levelFactory, boardFactory);
+           ArrayList<String> map = new ArrayList<>();
+           map.add("##########");
+           map.add("#P G#");
+           map.add("##########");
+           mapParser.parseMap(map);
+        });
+        Assertions.assertEquals("Input text lines are not of equal width.", thrown.getMessage());
     }
 }
